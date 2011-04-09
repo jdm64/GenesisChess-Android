92bbdd4e3e71ea6126c2f9a6be041a27a0ac9c48
Justin Madru <jdm64@gawab.com>
2011-04-09 00:50:15 -0700

    add NumberSpinner based on NumberPicker
    NumberSpinner is based on Google's NumberPicker from API 11.

---
+++ src/com/chess/genesis/NumberSpinnerButton.java
@@ -0,0 +1,109 @@
+/* source: http://android.git.kernel.org/?p=platform/frameworks/base.git;a=blob;f=core/java/android/widget/NumberPickerButton.java;hb=HEAD
+ * SICK! Added in API 11, renamed from NumberPickerButton to NumberSpinnerButton
+ *
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.chess.genesis;
+
+import android.content.Context;
+import android.text.method.NumberKeyListener;
+import android.text.Spanned;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+/**
+ * This class exists purely to cancel long click events, that got
+ * started in NumberPicker
+ */
+class NumberSpinnerButton extends ImageView
+{
+	private NumberSpinner mNumberSpinner;
+
+	public NumberSpinnerButton(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+		setAdjustViewBounds(true);
+		setScaleType(ImageView.ScaleType.CENTER_CROP);
+	}
+
+	public NumberSpinnerButton(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		setAdjustViewBounds(true);
+		setScaleType(ImageView.ScaleType.CENTER_CROP);
+	}
+
+	public NumberSpinnerButton(Context context)
+	{
+		super(context);
+		setAdjustViewBounds(true);
+		setScaleType(ImageView.ScaleType.CENTER_CROP);
+	}
+
+	public void setNumberSpinner(NumberSpinner picker)
+	{
+		mNumberSpinner = picker;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		cancelLongpressIfRequired(event);
+		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean onTrackballEvent(MotionEvent event)
+	{
+		cancelLongpressIfRequired(event);
+		return super.onTrackballEvent(event);
+	}
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event)
+	{
+		if ((keyCode == KeyEvent.KEYCODE_DPAD_CENTER) || (keyCode == KeyEvent.KEYCODE_ENTER)) {
+			cancelLongpress();
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+
+	private void cancelLongpressIfRequired(MotionEvent event) 
+	{
+		if ((event.getAction() == MotionEvent.ACTION_CANCEL) || (event.getAction() == MotionEvent.ACTION_UP)) {
+			cancelLongpress();
+		}
+	}
+
+	private void cancelLongpress()
+	{
+		if (R.id.increment == getId()) {
+			mNumberSpinner.cancelIncrement();
+		} else if (R.id.decrement == getId()) {
+			mNumberSpinner.cancelDecrement();
+		}
+	}
+
+	public void onWindowFocusChanged(boolean hasWindowFocus)
+	{
+		super.onWindowFocusChanged(hasWindowFocus);
+		if (!hasWindowFocus) {
+			cancelLongpress();
+		}
+	}
+}
