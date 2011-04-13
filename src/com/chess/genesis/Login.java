9e8005587d3b2e6d152ad741aa832511d23fbb85
Justin Madru <jdm64@gawab.com>
2011-04-12 20:58:19 -0700

    add missing trim() on the Login's username field
    missed a trim() on an EditText from the previous commit.

--- src/com/chess/genesis/Login.java
+++ src/com/chess/genesis/Login.java
@@ -48,7 +48,7 @@ public class Login extends Activity implements OnTouchListener, OnClickListener,
 					SocketClient.isLoggedin = true;
 
 					EditText txt = (EditText) findViewById(R.id.username);
-					final String username = txt.getText().toString();
+					final String username = txt.getText().toString().trim();
 
 					txt = (EditText) findViewById(R.id.password);
 					final String password = txt.getText().toString();
