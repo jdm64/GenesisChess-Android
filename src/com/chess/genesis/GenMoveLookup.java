d0a1890e89b6faf4c43041f2fd3b039711a3dc2c
Justin Madru <jdm64@gawab.com>
2011-09-26 20:17:40 -0700

    move duplicate code into new MoveLookup class
    GenMoveLookup and RegMoveLookup have identical functions and
    member variables. Move these into the new MoveLookup class
    and extend GenMoveLookup and RegMoveLookup from MoveLookup.

--- src/com/chess/genesis/GenMoveLookup.java
+++ src/com/chess/genesis/GenMoveLookup.java
@@ -1,72 +1,12 @@
 package com.chess.genesis;
 
-class GenMoveLookup
+class GenMoveLookup extends MoveLookup
 {
-	private static final int[] mailbox = {
-		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-		-1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
-		-1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
-		-1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
-		-1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
-		-1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
-		-1, 40, 41, 42, 43, 44, 45, 46, 47, -1,
-		-1, 48, 49, 50, 51, 52, 53, 54, 55, -1,
-		-1, 56, 57, 58, 59, 60, 61, 62, 63, -1,
-		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-
-	private static final int[] mailbox64 = {
-		21, 22, 23, 24, 25, 26, 27, 28,
-		31, 32, 33, 34, 35, 36, 37, 38,
-		41, 42, 43, 44, 45, 46, 47, 48,
-		51, 52, 53, 54, 55, 56, 57, 58,
-		61, 62, 63, 64, 65, 66, 67, 68,
-		71, 72, 73, 74, 75, 76, 77, 78,
-		81, 82, 83, 84, 85, 86, 87, 88,
-		91, 92, 93, 94, 95, 96, 97, 98};
-
-	private static final int[][] offsets = {
-		{  0,   0,   0,  0,   0,  0,  0,  0},
-		{-11,  -9,   9, 11, -10, -1,  1, 10},
-		{-21, -19, -12, -8,   8, 12, 19, 21},
-		{-11,  -9,   9, 11,   0,  0,  0,  0},
-		{-10,  -1,   1, 10,   0,  0,  0,  0},
-		{-11, -10,  -9, -1,   1,  9, 10, 11},
-		{-11, -10,  -9, -1,   1,  9, 10, 11} };
-
-	private final int[] square;
-
 	public GenMoveLookup(final int[] Square)
 	{
 		square = Square;
 	}
 
-	private boolean NOT_CAPTURE(final int A, final int B)
-	{
-		return (A * B >= 0);
-	}
-	
-	private boolean OWN_PIECE(final int A, final int B)
-	{
-		return (A * B >  0);
-	}
-
-	private boolean EMPTY_MOVE(final int A, final int B)
-	{
-		return (A * B == 0);
-	}
-	
-	private boolean CAPTURE_MOVE(final int A, final int B)
-	{
-		return (A * B <  0);
-	}
-
-	private boolean ANY_MOVE(final int A, final int B)
-	{
-		return (A * B <= 0);
-	}
-	
 	public int[] genAll(final int from)
 	{
 		final int type = Math.abs(square[from]), mfrom = mailbox64[from];
