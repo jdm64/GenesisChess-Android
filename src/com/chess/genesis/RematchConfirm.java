13304397ef263842c3897b2076b1f7629b0456f6
Justin Madru <jdm64@gawab.com>
2011-10-04 15:36:44 -0700

    add regular chess to RematchConfirm

--- src/com/chess/genesis/RematchConfirm.java
+++ src/com/chess/genesis/RematchConfirm.java
@@ -43,8 +43,8 @@ class RematchConfirm extends Dialog implements OnClickListener
 		button.setOnClickListener(this);
 
 		AdapterItem[] list = new AdapterItem[]
-			{new AdapterItem("Genesis", Enums.GENESIS_CHESS) };
-			// new AdapterItem("Regular", Enums.REGULAR_CHESS) };
+			{new AdapterItem("Genesis", Enums.GENESIS_CHESS),
+			new AdapterItem("Regular", Enums.REGULAR_CHESS) };
 
 		ArrayAdapter<AdapterItem> adapter = new ArrayAdapter<AdapterItem>(this.getContext(), android.R.layout.simple_spinner_item, list);
 		adapter.setDropDownViewResource(R.layout.spinner_dropdown);
