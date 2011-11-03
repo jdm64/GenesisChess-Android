5b81b499f972e1c789f7f6148837b2a293047195
Justin Madru <jdm64@gawab.com>
2011-11-03 01:25:00 -0700

    wait for polling freq if GenesisAlarm called from boot/upgrade
    Schedule another alarm instead of starting GenesisNotifier if
    GenesisAlarm was called from boot/upgrade.

--- src/com/chess/genesis/net/GenesisAlarm.java
+++ src/com/chess/genesis/net/GenesisAlarm.java
@@ -1,15 +1,25 @@
 package com.chess.genesis;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import java.util.Calendar;
 
 public class GenesisAlarm extends BroadcastReceiver
 {
 	@Override
 	public void onReceive(final Context context, final Intent intent)
 	{
+		if (intent.getAction() != null) {
+			ScheduleWakeup(context);
+			return;
+		}
+
 		final Intent nintent = new Intent(context, GenesisNotifier.class);
 		final Bundle bundle = new Bundle();
 
@@ -17,5 +27,18 @@ public class GenesisAlarm extends BroadcastReceiver
 		nintent.putExtras(bundle);
 
 		context.startService(nintent);
+	}
+
+	private void ScheduleWakeup(final Context context)
+	{
+		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+		final Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MINUTE, pref.getInt("notifierPolling", GenesisNotifier.POLL_FREQ));
+
+		final Intent intent = new Intent(context, GenesisAlarm.class);
+		final PendingIntent pintent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+		final AlarmManager am = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+		am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), pintent);
 	}
 }
