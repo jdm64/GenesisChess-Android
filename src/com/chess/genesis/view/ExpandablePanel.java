c3c4db3dec2b749ab0e8cc2d7ba84f4993cab060
Justin Madru <jdm64@gawab.com>
2011-12-19 23:45:58 -0800

    add ExpandablePanel class
    ExpandablePanel is a LinearLayout that hides/shows views with a click.
    The first child view becomes the header and is the only child shown
    on initial view. All other child view have their height set to zero.
    When the panel is clicked the view expands to show all views. If
    clicked again, the view collapses to only show the header/first view.

---
+++ src/com/chess/genesis/view/ExpandablePanel.java
@@ -0,0 +1,138 @@
+package com.chess.genesis;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
+import android.view.ViewGroup.LayoutParams;
+import android.util.Log;
+
+class ExpandablePanel extends LinearLayout implements OnClickListener
+{
+	private View top;
+	private int duration = 500;
+	private int totalHeight = 0;
+	private boolean isExpanded = false;
+	private Paint paint;
+
+	public ExpandablePanel(Context context)
+	{
+		this(context, null);
+	}
+
+	public ExpandablePanel(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+
+		paint = new Paint();
+		paint.setColor(0xff00b7eb);
+	}
+
+	@Override
+	protected void onFinishInflate()
+	{
+		super.onFinishInflate();
+
+		final int count = getChildCount();
+
+		if (count < 1)
+			return;
+
+		top = getChildAt(0);
+		top.measure(getWidth(), MeasureSpec.UNSPECIFIED);
+		totalHeight = top.getMeasuredHeight();
+
+		if (count < 2)
+			return;
+
+		for (int i = 1; i < count; i++) {
+			final View child = getChildAt(i);
+
+			child.measure(getWidth(), MeasureSpec.UNSPECIFIED);
+			totalHeight += child.getMeasuredHeight();
+
+			final android.view.ViewGroup.LayoutParams lp = child.getLayoutParams();
+			lp.height = 0;
+
+			child.setLayoutParams(lp);
+			child.setVisibility(View.GONE);
+		}
+		setOnClickListener(this);
+	}
+
+	@Override
+	public void dispatchDraw(final Canvas canvas)
+	{
+		super.dispatchDraw(canvas);
+
+		canvas.drawRect(0, getHeight() - 4, getWidth(), getHeight(), paint);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		if (isExpanded) {
+			for (int i = 1; i < getChildCount(); i++) {
+				View item = getChildAt(i);
+
+				final ExpandAnimation a = new ExpandAnimation(item, item.getHeight(), 0);
+				a.setDuration(duration);
+				item.startAnimation(a);
+			}
+			final ExpandAnimation a = new ExpandAnimation(this, getHeight(), top.getHeight());
+			a.setDuration(duration);
+			startAnimation(a);
+		} else {
+			final ExpandAnimation b = new ExpandAnimation(this, getHeight(), totalHeight);
+			b.setDuration(duration);
+			startAnimation(b);
+
+			for (int i = getChildCount() - 1; i > 0; i--) {
+				View item = getChildAt(i);
+				item.measure(getWidth(), MeasureSpec.UNSPECIFIED);
+				final int height = item.getMeasuredHeight();
+
+				final ExpandAnimation a = new ExpandAnimation(item, 0, height);
+				a.setDuration(duration);
+				item.startAnimation(a);
+			}
+		}
+		isExpanded = !isExpanded;
+	}
+
+	private class ExpandAnimation extends Animation
+	{
+		private final View item;
+		private final int start;
+		private final int delta;
+
+		public ExpandAnimation(View view, int startHeight, int endHeight)
+		{
+			item = view;
+			item.setVisibility(View.VISIBLE);
+
+			start = startHeight;
+			delta = endHeight - start;
+		}
+
+		@Override
+		protected void applyTransformation(float interpolatedTime, Transformation t)
+		{
+			android.view.ViewGroup.LayoutParams lp = item.getLayoutParams();
+			lp.height = (int) (start + delta * interpolatedTime);
+
+			item.setLayoutParams(lp);
+		}
+
+		@Override
+		public boolean willChangeBounds()
+		{
+			return true;
+		}
+	}
+}
