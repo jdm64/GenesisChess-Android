0708422f3d69c7da0512914e9d2ea2fb7dd0ece3
Justin Madru <jdm64@gawab.com>
2012-03-24 01:48:55 -0700

    manually draw msglist items using MsgListItem class
    to further reduce the number of views created, manually draw
    the msglist item views. MsgListItem has a private static
    class, Cache, to hold the common constants used in creating
    the item views.

--- src/com/chess/genesis/widget/MsgListAdapter.java
+++ src/com/chess/genesis/widget/MsgListAdapter.java
@@ -1,14 +1,22 @@
 package com.chess.genesis;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteCursor;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.Layout.Alignment;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
-import android.widget.TextView;
 
 class MsgListAdapter extends BaseAdapter implements ListAdapter
 {
@@ -18,13 +26,6 @@ class MsgListAdapter extends BaseAdapter implements ListAdapter
 	private GameDataDB db;
 	private SQLiteCursor list;
 
-	static final class MsgHolder
-	{
-		public TextView username;
-		public TextView time;
-		public TextView msg;
-	}
-
 	public MsgListAdapter(final Context _context, final String GameID)
 	{
 		super();
@@ -32,6 +33,11 @@ class MsgListAdapter extends BaseAdapter implements ListAdapter
 		context = _context;
 		gameID = GameID;
 		initCursor();
+
+		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
+		final String username = pref.getString("username", "");
+
+		MsgListItem.Cache.Init(context, username);
 	}
 
 	private void initCursor()
@@ -73,36 +79,11 @@ class MsgListAdapter extends BaseAdapter implements ListAdapter
 	public View getView(final int index, View cell, final ViewGroup parent)
 	{
 		final Bundle data = (Bundle) getItem(index);
-		final MsgHolder holder;
 
-		if (cell == null) {
-			holder = new MsgHolder();
-			cell = setupMsg(parent, holder);
-		} else {
-			holder = (MsgHolder) cell.getTag();
-		}
-		reloadMsg(data, holder);
+		cell = new MsgListItem(parent.getContext());
+		((MsgListItem) cell).setData(data);
 
 		return cell;
-	}
-
-	private View setupMsg(final ViewGroup parent, final MsgHolder holder)
-	{
-		final View cell = View.inflate(parent.getContext(), R.layout.msglist_cell, null);
-
-		holder.username = (TextView) cell.findViewById(R.id.username);
-		holder.time = (TextView) cell.findViewById(R.id.time);
-		holder.msg = (TextView) cell.findViewById(R.id.msg);
-		cell.setTag(holder);
-
-		return cell;
-	}
-
-	private void reloadMsg(final Bundle data, final MsgHolder holder)
-	{
-		holder.username.setText(data.getString("username"));
-		holder.time.setText(new PrettyDate(data.getString("time")).agoFormat());
-		holder.msg.setText(data.getString("msg"));
 	}
 
 	public View getEmptyView(final Context context)
@@ -114,5 +95,110 @@ class MsgListAdapter extends BaseAdapter implements ListAdapter
 		cell.setLayoutParams(lp);
 
 		return cell;
+	}
+}
+
+class MsgListItem extends View
+{
+	private final TextPaint paint = new TextPaint();
+	private Bundle data;
+	private StaticLayout msgImg;
+	private PrettyDate time;
+	private int lastWidth;
+
+	final static class Cache
+	{
+		public static Typeface fontNormal;
+		public static Typeface fontItalic;
+		public static String username;
+		public static int dpi;
+
+		public static int padding;
+		public static int smallText;
+		public static int largeText;
+		public static int headerAlign;
+		public static int headerHeight;
+
+		private Cache()
+		{
+		}
+
+		public static void Init(final Context context, final String Username)
+		{
+			fontNormal = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Regular.ttf");
+			fontItalic = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Italic.ttf");
+			username = Username;
+
+			final DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+			dpi = (int) ((1 + Math.max(metrics.ydpi, metrics.xdpi)) / 160);
+
+			padding = 9 * dpi;
+			smallText = 20 * dpi;
+			largeText = 22 * dpi;
+			headerAlign = 22 * dpi;
+			headerHeight = 30 * dpi;
+		}
+	}
+
+	public MsgListItem(final Context context)
+	{
+		super(context);
+
+		paint.setAntiAlias(true);
+		paint.setTypeface(Cache.fontNormal);
+		paint.setTextSize(Cache.largeText);
+	}
+
+	@Override
+	protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec)
+	{
+		final int width = MeasureSpec.getSize(widthMeasureSpec);
+
+		if (width != lastWidth) {
+			msgImg = new StaticLayout(data.getString("msg"), paint, width - 4 * Cache.padding, Alignment.ALIGN_NORMAL, 1, 0, true);
+			lastWidth = width;
+		}
+
+		setMeasuredDimension(width, msgImg.getHeight() + Cache.headerHeight + 2 * Cache.padding);
+	}
+
+	@Override
+	protected void onDraw(final Canvas canvas)
+	{
+		final int width = getMeasuredWidth();
+
+		// draw msg text
+		canvas.save();
+		canvas.translate(2 * Cache.padding, Cache.headerHeight);
+		msgImg.draw(canvas);
+		canvas.restore();
+
+		// draw msg header
+		if (data.getString("username").equals(Cache.username))
+			paint.setColor(0xffd2d0ff);
+		else
+			paint.setColor(0xffcce6ff);
+		canvas.drawRect(0, 0, getWidth(), Cache.headerHeight, paint);
+
+		// draw username
+		paint.setColor(0xff000000);
+		paint.setTextSize(Cache.smallText);
+		canvas.drawText(data.getString("username"), Cache.padding, Cache.headerAlign, paint);
+
+		// draw time
+		paint.setTextAlign(Paint.Align.RIGHT);
+		paint.setTypeface(Cache.fontItalic);
+		canvas.drawText(time.agoFormat(), width - Cache.padding, Cache.headerAlign, paint);
+
+		// reset paint
+		paint.setTextAlign(Paint.Align.LEFT);
+		paint.setTypeface(Cache.fontNormal);
+		paint.setTextSize(Cache.largeText);
+	}
+
+	public void setData(final Bundle bundle)
+	{
+		data = bundle;
+		time = new PrettyDate(data.getString("time"));
 	}
 }
