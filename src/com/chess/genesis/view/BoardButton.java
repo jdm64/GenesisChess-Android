44a26fa2179883369a90aa07b9d5ae49a9672e4d
Justin Madru <justin.jdm64@gmail.com>
2012-04-24 21:53:12 -0700

    move [Board|Place]ButtonCache into [Board|Place]Button
    to reduce file clutter.

--- src/com/chess/genesis/view/BoardButton.java
+++ src/com/chess/genesis/view/BoardButton.java
@@ -150,3 +150,41 @@ class BoardButton extends View
 		invalidate();
 	}
 }
+
+final class BoardButtonCache extends PieceCache
+{
+	private static Bitmap[] scaledPieces;
+	private static int size;
+	private static boolean hasInit = false;
+
+	private BoardButtonCache()
+	{
+	}
+
+	public static void Init(final Context context)
+	{
+		InitPieces(context);
+
+		if (hasInit)
+			return;
+
+		size = 0;
+		hasInit = true;
+		scaledPieces = new Bitmap[13];
+	}
+
+	public static void resizeImages(final int newSize)
+	{
+		if (newSize == size || newSize < 1)
+			return;
+
+		size = newSize;
+		for (int i = 0; i < 13; i++)
+			scaledPieces[i] = createImg(i, size);
+	}
+
+	public static Bitmap getPieceImg(final int index)
+	{
+		return scaledPieces[index];
+	}
+}
--- src/com/chess/genesis/view/PlaceButton.java
+++ src/com/chess/genesis/view/PlaceButton.java
@@ -17,6 +17,8 @@
 package com.chess.genesis;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
@@ -130,5 +132,71 @@ class PlaceButton extends View
 	{
 		isHighlighted = mode;
 		invalidate();
+	}
+}
+
+final class PlaceButtonCache extends PieceCache
+{
+	private static final int[] countImages = {
+		R.drawable.piece_0,	R.drawable.piece_1,	R.drawable.piece_2,
+		R.drawable.piece_3,	R.drawable.piece_4,	R.drawable.piece_5,
+		R.drawable.piece_6,	R.drawable.piece_7,	R.drawable.piece_8,
+		R.drawable.piece_9};
+
+	private static Bitmap[] scaledCounts;
+	private static Bitmap[] scaledPieces;
+	private static Bitmap scaledToken;
+
+	private static int size;
+	private static boolean hasInit = false;
+
+	private PlaceButtonCache()
+	{
+	}
+
+	public static void Init(final Context context)
+	{
+		InitPieces(context);
+
+		if (hasInit)
+			return;
+
+		size = 0;
+		hasInit = true;
+		scaledPieces = new Bitmap[13];
+		scaledCounts = new Bitmap[10];
+	}
+
+	public static void resizeImages(final int newSize)
+	{
+		if (newSize == size || newSize < 1)
+			return;
+
+		size = newSize;
+		for (int i = 0; i < 13; i++)
+			scaledPieces[i] = createImg(i, size);
+
+		final Bitmap[] countBitmaps = new Bitmap[10];
+		for (int i = 0; i < 10; i++) {
+			countBitmaps[i] = BitmapFactory.decodeResource(cntx.getResources(), countImages[i]);
+			scaledCounts[i] = Bitmap.createScaledBitmap(countBitmaps[i], size, size, true);
+		}
+		final Bitmap tokenBitmap = BitmapFactory.decodeResource(cntx.getResources(), R.drawable.piece_token);
+		scaledToken = Bitmap.createScaledBitmap(tokenBitmap, size, size, true);
+	}
+
+	public static Bitmap getPieceImg(final int index)
+	{
+		return scaledPieces[index];
+	}
+
+	public static Bitmap getCountImg(final int count)
+	{
+		return scaledCounts[count];
+	}
+
+	public static Bitmap getTokenImg()
+	{
+		return scaledToken;
 	}
 }
