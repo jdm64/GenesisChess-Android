83dae949d35edfa9f812d08e5a79164d035995ab
Justin Madru <justin.jdm64@gmail.com>
2013-05-18 17:26:54 -0700

    update MColors to use colors closer to Android's Design Colors
    All the colors except one (blue) use duplicate hex values for each
    color channel. For example, instead of 0x33b5e5 use 0x33bbee. Modify
    the colors in MColors to use this pattern.
    
    Android's Design Colors: http://developer.android.com/design/style/color.html

--- src/com/chess/genesis/data/MColors.java
+++ src/com/chess/genesis/data/MColors.java
@@ -22,30 +22,37 @@ public final class MColors
 	{
 	}
 
+	/*
+	Android Style Colors
+	R=0..3..6..9A.C..F
+	G=0..34.6.89.BC...
+	B=0..34.......C.E.
+	*/
+
 	public final static int WHITE = 0xffffffff;
 	public final static int BLACK = 0xff000000;
 	public final static int CLEAR = 0x00ffffff;
-	public final static int GREY  = 0xff808080;
-	public final static int GREY_LIGHT = 0xffd3d3d3;
+	public final static int GREY  = 0xff888888;
+	public final static int GREY_LIGHT = 0xffcccccc;
 
-	public final static int RED = 0xffeb0000;
-	public final static int RED_LIGHT = 0xffdf4c37;
+	public final static int RED = 0xffee0000;
+	public final static int RED_LIGHT = 0xffee4433;
 	public final static int RED_DARK = 0xffcc0000;
 
-	public final static int ORANGE = 0xfff57900;
+	public final static int ORANGE = 0xffff8800;
 
-	public final static int GREEN_TEAL = 0xff36b54a;
-	public final static int GREEN_DARK = 0xff4e9a06;
+	public final static int GREEN_TEAL = 0xff33bb44;
+	public final static int GREEN_DARK = 0xff449900;
 
-	public final static int TEAL_PASTEL = 0xffe2f4fb;
+	public final static int TEAL_PASTEL = 0xffeeffff;
 
-	public final static int BLUE_NEON_TR = 0x8000b7eb;
-	public final static int BLUE_NEON = 0xff00b7eb;
-	public final static int BLUE_DARK = 0xff3465a4;
-	public final static int BLUE_NAVY = 0xff6885b4;
-	public final static int BLUE_NAVY_DARK = 0xff5a7397;
-	public final static int BLUE_PASTEL = 0xffcce6ff;
+	public final static int BLUE_NEON_TR = 0x8800bbee;
+	public final static int BLUE_NEON = 0xff00bbee;
+	public final static int BLUE_DARK = 0xff3366aa;
+	public final static int BLUE_NAVY = 0xff6688bb;
+	public final static int BLUE_NAVY_DARK = 0xff446699;
+	public final static int BLUE_PASTEL = 0xffcceeff;
 
-	public final static int PURPLE_LIGHT = 0xffab9aca;
-	public final static int PURPLE_PASTEL = 0xffd2d0ff;
+	public final static int PURPLE_LIGHT = 0xffaa99cc;
+	public final static int PURPLE_PASTEL = 0xffddddff;
 }
