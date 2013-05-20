jdm64/GenesisChess-Android

commit 93a4ef33bda770be608f530be605b81d8add65a4

static analysis from Eclipse and other code clean ups
Eclipse has many static analysis rules that PMD didn't catch.
Other simple code formatting changes are included.

src/com/chess/genesis/view/NumberSpinner.java
@@ -341,16 +341,16 @@ public class NumberSpinner extends LinearLayout
 	*
 	* @param current the new value of the NumberSpinner
 	*/
-	protected void changeCurrent(int current)
+	protected void changeCurrent(final int current)
 	{
+		mPrevious = mCurrent;
+
 		// Wrap around the values if we go past the start or end
 		if (current > mEnd) {
-			current = mStart;
+			mCurrent = mStart;
 		} else if (current < mStart) {
-			current = mEnd;
+			mCurrent = mEnd;
 		}
-		mPrevious = mCurrent;
-		mCurrent = current;
 		notifyChange();
 		updateView();
 	}
@@ -495,11 +495,9 @@ public class NumberSpinner extends LinearLayout
 			 * as the user might want to delete some numbers
 			 * and then type a new number.
 			 */
-			if (val > mEnd) {
+			if (val > mEnd)
 				return "";
-			} else {
-				return filtered;
-			}
+			return filtered;
 		}
 	}
 

commit fbe3997d99ae6f60062d08da333aa9dd7efc962f

fix regression in NumberSpinner
commit 93a4ef33bda770be608f530be605b81d8add65a4 caused the number
spinner to stop working. mCurrent wasn't being set when current
was in the range mEnd - mStart (which was always).

src/com/chess/genesis/view/NumberSpinner.java
@@ -350,6 +350,8 @@ public class NumberSpinner extends LinearLayout
 			mCurrent = mStart;
 		} else if (current < mStart) {
 			mCurrent = mEnd;
+		} else {
+			mCurrent = current;
 		}
 		notifyChange();
 		updateView();