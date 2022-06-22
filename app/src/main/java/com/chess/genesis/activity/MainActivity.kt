65275899003961b08e9202160b145da7912b3c80
Justin Madru <justin.jdm64@gmail.com>
2022-06-21 20:43:07 -0700

    load the last used page when starting

--- app/src/main/java/com/chess/genesis/activity/MainActivity.kt
+++ app/src/main/java/com/chess/genesis/activity/MainActivity.kt
@@ -15,14 +15,23 @@
  */
 package com.chess.genesis.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.chess.genesis.R
+import com.chess.genesis.data.Pref
+import com.chess.genesis.data.PrefEdit
 import com.chess.genesis.db.LocalGameDao
 import com.chess.genesis.net.AdhocMqttClient
 import kotlinx.coroutines.Dispatchers
@@ -67,15 +76,38 @@ class MainActivity : ComponentActivity() {
 fun MainApp() {
 	var nav = rememberNavController()
 
+	nav.addOnDestinationChangedListener { _, _, bundle ->
+		val intent = bundle?.get("android-support-nav:controller:deepLinkIntent")
+		if (intent is Intent) {
+			val path = intent.data?.path?.substring(1)
+			if (path != null) {
+				PrefEdit(nav.context).putString(R.array.pf_lastpage, path).commit()
+			}
+		}
+	}
+
 	MaterialTheme {
-		NavHost(nav, "list") {
+		NavHost(nav, "start") {
+			composable("start") { LoadingPage(nav) }
+			composable("list") { GameListPage(nav) }
 			composable("board/{gameId}") { entry ->
 				var id = entry.arguments?.getString("gameId")
 				if (id != null) {
 					GamePage(nav, id)
 				}
 			}
-			composable("list") { GameListPage(nav) }
 		}
+	}
+}
+
+@Composable
+fun LoadingPage(nav: NavHostController) {
+	Box(Modifier.fillMaxSize())
+
+	LaunchedEffect(1) {
+		Thread.sleep(125)
+		val page = Pref.getString(nav.context, R.array.pf_lastpage)
+		nav.popBackStack()
+		nav.navigate(page)
 	}
 }
